d976ad1757df14982ce8a7d23adaef9e1776f6f4
Chi Señires <chiara.sen@ascendcorp.com>
2019-01-13 01:04:21 +0800

    Set up hero banner carousel - added 2 more placeholder background images for hero banner - slider is in place to demo what it looks like on actual site

---
+++ main.js
@@ -0,0 +1,13 @@
+// Custom JS for Rare Cancer PH
+$(document).ready(function() {
+  $('.carousel').lightSlider({
+    item: 1,
+    autoWidth: false,
+    auto: true,
+    loop: true,
+    pause: 5000,
+    pauseOnHover: true,
+    enableTouch: false,
+    enableDrag: false,
+  });
+});
