eebd13f08744e9f838ee6fbdc01f1e10ce9d2f02
Chi Señires <chiara.sen@ascendcorp.com>
2019-03-31 11:39:26 +0800

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+/* Stages template */
+
+get_header(); ?>
+
+<main>
+  <section class="hero-banner text-only">
+    <div class="hero-text">
+      <h1>Page not found</h1>
+      <div class="hero-description">Sorry!</div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
