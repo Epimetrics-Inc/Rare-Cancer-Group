fbbec5cce8bcf53dfe2f805810924ef00876a7ef
Chi Señires <chiara.sen@ascendcorp.com>
2019-03-31 14:23:07 +0800

    Fixed Search Query loop

--- search.php
+++ search.php
@@ -18,17 +18,18 @@ get_header(); ?>
         <?php _e( 'Search Results for', 'locale' ); ?>: "<?php the_search_query(); ?>"</h1>
       </div>
       <div class="article-cards">
-        <?php query_posts('showposts=3'); if (have_posts()) : ?>
-        <div class="article-card medical-information">
-          <div class="article-card-heading">
-            <h3 class="article-title"><?php the_title(); ?></h3>
-            <p class="article-meta"><?php the_date( 'j F Y' ) ?> | <?php the_time( 'H:i A' ); ?></p>
-          </div>
-          <p class="article-excerpt"><?php echo substr(get_the_excerpt(), 0,200); ?></p>
-          <a href="<?php echo get_permalink(); ?>" class="article-read-more">Read more &raquo;</a>
-          <?php $article_category = get_the_category(); ?>
-          <a href="<?php echo esc_url( get_category_link( $article_category[0]->cat_ID ) ); ?>" class="article-card-category"><?php echo $article_category[0]->name; ?></a>
-        </div>
+        <?php if (have_posts()) : ?>
+          <?php while( have_posts() ) : the_post(); ?>
+            <?php if ( get_the_category()[0] != null ) : ?>
+              <div class="article-card <?php echo get_the_category()[0]->slug; ?>">
+                <?php the_title( '<h3 class="article-title">', '</h3>' ); ?>
+                <p class="article-meta"><?php the_date( 'j F Y' ) ?> | <?php the_time( 'H:i A' ); ?></p>
+                <div class="article-excerpt"><?php echo wp_trim_words( get_the_content(), 40, '...' ); ?></div>
+                <a href="<?php the_permalink(); ?>" class="article-read-more">Read more &raquo;</a>
+                <a href="<?php echo esc_url( get_category_link( get_the_category()[0]->cat_ID ) ); ?>" class="article-card-category"><?php echo get_the_category()[0]->name; ?></a>
+              </div>
+            <?php endif; ?>
+          <?php endwhile; ?>
         <?php endif; ?>
       </div>
     </section>
